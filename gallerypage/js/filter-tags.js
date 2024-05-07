aaaa4e54bb065fec06a2e481d0f586f889c48887
siva <siva2122001@gmail.com>
2024-05-07 15:24:23 +0530

    07/05 update

---
+++ gallerypage/js/filter-tags.js
@@ -0,0 +1,66 @@
+(function() {
+
+  var $imgs = $('#gallery img');
+  var $buttons = $('#buttons');
+  var tagged = {};
+
+  $imgs.each(function() {
+    var img = this;
+    var tags = $(this).data('tags');
+
+    if (tags) {
+      tags.split(',').forEach(function(tagName) {
+        if (tagged[tagName] == null) {
+          tagged[tagName] = [];
+        }
+        tagged[tagName].push(img);
+      });
+    }
+  });
+
+  $('<button/>', {
+    text: 'Show All',
+    class: 'active',
+    click: function() {
+      $(this)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+      $imgs.show();
+    }
+  }).appendTo($buttons);
+
+  $.each(tagged, function(tagName) {
+    $('<button/>', {
+      text: tagName + ' (' + tagged[tagName].length + ')',
+      click: function() {
+        $(this)
+          .addClass('active')
+          .siblings()
+          .removeClass('active');
+        $imgs
+          .hide()
+          .filter(tagged[tagName])
+          .show();
+      }
+    }).appendTo($buttons);
+  });
+
+}());
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
--- javascript/model.js
+++ javascript/model.js
@@ -1,12 +1,12 @@
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "20px";
-    } else {
-      document.getElementById("navbar").style.top = "-500px";
-    }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "20px";
+//     } else {
+//       document.getElementById("navbar").style.top = "-500px";
+//     }
+// }
 
 
 document.addEventListener('contextmenu', (e) => e.preventDefault());
@@ -25,3 +25,11 @@ document.onkeydown = (e) => {
   )
     return false;
 };
+
+
+function PageLoad()
+{
+  window.alert('Leave Site?');
+}
+
+
